e6cb1bcf79e2f6828856be2bed2066271ed3d3e1
Sergei Poluboiarinov <sihtmark@gmail.com>
2023-08-25 15:37:34 +0300

    Initial Commit

---
+++ TestDikidi/TestDikidiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDikidiApp.swift
+//  TestDikidi
+//
+//  Created by sihtmark on 25.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestDikidiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
