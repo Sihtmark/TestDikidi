729d8c565b134ad2850e72b79f85cc572f92062e
Sergei Poluboiarinov <sihtmark@gmail.com>
2023-08-28 19:09:24 +0300

    DIKIDI-5

--- TestDikidi/Managers/NetworkingManager/ResponseModels.swift
+++ TestDikidi/Managers/NetworkingManager/ResponseModels.swift
@@ -8,115 +8,231 @@
 import Foundation
 
 struct ResponseModel: Codable {
-    let errorMessage: ErrorMessage
-    let data: MainPageData
+    let error: ErrorStruct
+    let data: DataClass
     
-    private enum CodingKeys: String, CodingKey {
-        case errorMessage = "error"
+    enum CodingKeys: String, CodingKey {
+        case error = "error"
         case data = "data"
     }
 }
 
-struct ErrorMessage: Codable {
-    let code: Int
-    let message: String?
-    
-    private enum CodingKeys: String, CodingKey {
-        case code = "code"
-        case message = "message"
-    }
-}
-
-struct MainPageData: Codable {
+struct DataClass: Codable {
     let title: String
-    let mainImageURL: String
+    let image: String
     let catalogCount: String
-    let blocks: Blocks
+    let blocks: BlocksStruct
     let order: [String]
-    
-    private enum CodingKeys: String, CodingKey {
+
+    enum CodingKeys: String, CodingKey {
         case title = "title"
-        case mainImageURL = "image"
+        case image = "image"
         case catalogCount = "catalog_count"
         case blocks = "blocks"
         case order = "order"
     }
 }
 
-struct Blocks: Codable {
-    let vip: [VIPUnit]
+struct BlocksStruct: Codable {
+//    let favorites: [Favorite]?
+//    let categories: [Category]?
+    let vip: [Favorite]
+    let shares: Shares
+//    let popular: [Popular]?
+//    let certificates: [JSONAny]
     let examples: String
-    let catalog: [CatalogUnit]
+//    let new: [Favorite]?
+    let catalog: [Catalog]
+    let examples2: String
     
-    private enum CodingKeys: String, CodingKey {
+    enum CodingKeys: String, CodingKey {
+//        case favorites = "favorites"
+//        case categories = "categories"
         case vip = "vip"
+        case shares = "shares"
+//        case popular = "popular"
+//        case certificates = "certificates"
         case examples = "examples"
+//        case new = "new"
         case catalog = "catalog"
+        case examples2 = "examples2"
     }
 }
 
-struct VIPUnit: Codable, Identifiable {
-    let id: String
-    let images: ImageUnit
-    let name: String
-    let categories: [String]?
-    
-    private enum CodingKeys: String, CodingKey {
-        case id = "id"
-        case images = "image"
-        case name = "name"
-        case categories = "categories"
-    }
-}
-
-struct ImageUnit: Codable {
-    let thumbURL: String
-    let originURL: String
-    
-    private enum CodingKeys: String, CodingKey {
-        case thumbURL = "thumb"
-        case originURL = "origin"
-    }
-}
-
-struct CatalogUnit: Codable, Identifiable {
+struct Catalog: Codable {
     let id: String
     let name: String
-    let images: ImageUnit
+    let image: ImageStruct
     let street: String
     let house: String
+//    let schedule: ScheduleUnion
     let lat: String
     let lng: String
-    let categories: [String]?
+    let categories: [String]
     let rating: Double
     let isMaster: Bool
-    let currency: CurrencyUnit
-    let masterID: String?
-    
-    private enum CodingKeys: String, CodingKey {
+    let currency: Currency
+//    let distance: String?
+//    let masterID: String?
+
+    enum CodingKeys: String, CodingKey {
         case id = "id"
         case name = "name"
-        case images = "image"
+        case image = "image"
         case street = "street"
         case house = "house"
+//        case schedule = "schedule"
         case lat = "lat"
         case lng = "lng"
         case categories = "categories"
         case rating = "rating"
         case isMaster = "isMaster"
         case currency = "currency"
-        case masterID = "master_id"
+//        case distance = "distance"
+//        case masterID = "master_id"
     }
 }
 
-struct CurrencyUnit: Codable {
+struct Currency: Codable {
     let id: String
     let title: String
     let abbr: String
     
-    private enum CodingKeys: String, CodingKey {
+    enum CodingKeys: String, CodingKey {
         case id = "id"
         case title = "title"
         case abbr = "abbr"
+    }
+}
+
+struct ImageStruct: Codable {
+    let thumb: String
+    let origin: String
+    
+    enum CodingKeys: String, CodingKey {
+        case thumb = "thumb"
+        case origin = "origin"
+    }
+}
+
+struct ScheduleElement: Codable {
+    let day: String
+    let workFrom: String
+    let workTo: String
+
+    enum CodingKeys: String, CodingKey {
+        case day = "day"
+        case workFrom = "work_from"
+        case workTo = "work_to"
+    }
+}
+
+struct Category: Codable {
+    let id: String
+    let name: String
+    let image: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case image = "image"
+    }
+}
+
+struct Favorite: Codable {
+    let id: String
+    let image: ImageStruct
+    let name: String
+    let categories: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case image = "image"
+        case name = "name"
+        case categories = "categories"
+    }
+}
+
+struct Popular: Codable {
+    let id: String
+    let name: String
+    let image: ImageStruct
+    let street: String
+    let house: String
+    let schedule: [ScheduleElement]
+    let lat: String
+    let lng: String
+//    let categories: [JSONAny]
+    let rating: Int
+    let isMaster: Bool
+    let currency: Currency
+    let distance: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case image = "image"
+        case street = "street"
+        case house = "house"
+        case schedule = "schedule"
+        case lat = "lat"
+        case lng = "lng"
+//        case categories = "categories"
+        case rating = "rating"
+        case isMaster = "isMaster"
+        case currency = "currency"
+        case distance = "distance"
+    }
+}
+
+struct Shares: Codable {
+    let list: [List]
+    let count: String
+    
+    enum CodingKeys: String, CodingKey {
+        case list = "list"
+        case count = "count"
+    }
+}
+
+struct List: Codable {
+    let id: String
+    let name: String
+    let timeStart: String
+    let timeStop: String
+    let discountValue: String
+    let view: String
+    let usedCount: String
+    let companyID: String
+    let icon: String
+    let companyName: String
+    let companyStreet: String
+    let companyHouse: String
+    let companyImage: String
+
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case timeStart = "time_start"
+        case timeStop = "time_stop"
+        case discountValue = "discount_value"
+        case view = "view"
+        case usedCount = "used_count"
+        case companyID = "company_id"
+        case icon = "icon"
+        case companyName = "company_name"
+        case companyStreet = "company_street"
+        case companyHouse = "company_house"
+        case companyImage = "company_image"
+    }
+}
+
+struct ErrorStruct: Codable {
+    let code: Int
+//    let message: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case code = "code"
+//        case message = "message"
     }
 }
